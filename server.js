593e93df1deea1978abf43fd6d5ecab8738ebed5
Christofer <christoferguima@gmail.comnam>
2021-09-10 17:32:11 -0300

    server reading css and js aswell

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ const path = require('path');
 const port = 5000;
 
 http.createServer((req,res) => {
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+    
+    const file = req.url === '/' ? 'index.html' : req.url;
+    const filePath = path.join(__dirname, 'public', file);
+    const extname = path.extname(filePath);
+
+    const allowedFileTypes = ['.html', '.css', '.js']
+    const allowed = allowedFileTypes.find(item => item == extname);
+
+    if(!allowed) return
+
+    fs.readFile(
+            filePath, (err, content) => {
             if (err) {
                 throw err
             }
-            res.end(content)
-            
+            res.end(content);  
         })
-    }
 }).listen(port, () => console.log(`Server is running.. port: ${port}`));
